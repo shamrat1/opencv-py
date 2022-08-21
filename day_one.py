c574341a8fdfbe5ca9abdc00e60b31d6cda39ea0
mt-yasin <96203359+mt-shamrat@users.noreply.github.com>
2022-08-21 13:26:46 +0600

    rescale video and image

--- day_one.py
+++ day_one.py
@@ -1,17 +1,27 @@
+import re
+from turtle import width
 import cv2 as cv
 
 
 #108803
 
-img = cv.imread("assets/Photos/cat.jpg")
-cv.imshow("Cat",img)
-cv.waitKey(0)
+def rescaleFrame(frame, scale = 0.50):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width,height)
+    
+    return cv.resize(frame,dimensions,interpolation=cv.INTER_AREA)
 
-capture = cv.VideoCapture("assets/Videos/dog.mp4")
+# img = cv.imread("assets/Photos/cat.jpg")
+# cv.imshow("Cat",img)
+# cv.waitKey(0)
+
+capture = cv.VideoCapture(0)
 
 while True:
     isTrue, frame = capture.read()
-    cv.imshow('Video',frame)
+    resized = rescaleFrame(frame,2)
+    cv.imshow('Video',resized)
     
     if cv.waitKey(20) & 0xFF==ord('d'):
         break
